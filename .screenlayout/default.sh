bd5e8452f77fdd42b2ed7736233e7ecb541a8a3f
Connor Younglund <cdy@airmail.cc>
2018-11-29 12:50:00 -0500

    initial commit

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DVI-D-0 --primary --mode 1920x1080 --rate 144 --pos 1920x40 --rotate normal --output DVI-D-1 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal
